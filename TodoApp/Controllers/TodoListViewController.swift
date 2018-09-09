e15be9c83e7f099d68d48295ab96c71bfd633fb6
Irfaane <ousseny.irfaane@gmail.com>
2018-09-09 16:45:48 +0200

    Adding the search bar to query the database

--- TodoApp/Controllers/TodoListViewController.swift
+++ TodoApp/Controllers/TodoListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
@@ -16,18 +17,12 @@ class TodoListViewController: UITableViewController {
     
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //Persistance local data storage using UserDefaults
-   /*     if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = items
-        }
-    */
-        
         loadDataItems()
-        
-        
     }
 
     //TableView Datasource Methods
@@ -57,9 +52,12 @@ class TodoListViewController: UITableViewController {
     //TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        //CRUD Version : Deleting data from the permanent storage of the phone
+       // context.delete(itemArray[indexPath.row]) //-> Remove data from the permanent store
+       // itemArray.remove(at: indexPath.row) //-> Remove data from the itemArray
+
         saveDataItem()
         
         //selected a cell -> cell is gray
@@ -80,8 +78,9 @@ class TodoListViewController: UITableViewController {
             //what will happen once the user have pressed the button
             // -> we add at the end of the itemArray the new item we create
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false // we have to specify this field because in the DataModel, we set the done attributes as not an optionnal
             
             self.itemArray.append(newItem)
            
@@ -89,14 +88,11 @@ class TodoListViewController: UITableViewController {
             //self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.saveDataItem()
-            
         }
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
-            
-
         }
         
         alert.addAction(alertAction)
@@ -109,18 +105,39 @@ class TodoListViewController: UITableViewController {
     //Model manipulation data
     
     private func saveDataItem() {
+        //CRUD Version : Here we Create Data
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+        tableView.reloadData()
+    }
+    
+    func loadDataItems(with request : NSFetchRequest<Item> = Item.fetchRequest()) {
+        //CRUD Version : Here we Read Data from the database
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context : \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+/*    private func staDataItemV1() {
+        //Version 1 : using a Codable class
+
         let encoder = PropertyListEncoder()
         
         do {
             let data = try encoder.encode(itemArray)
             try data.write(to: dataFilePath!)
+            
         } catch {
             print(error)
         }
-        
-        tableView.reloadData()
     }
-    
+ 
     func loadDataItems() {
         if let data = try? Data(contentsOf: dataFilePath!) {
             let decoder = PropertyListDecoder()
@@ -131,8 +148,54 @@ class TodoListViewController: UITableViewController {
             }
         }
     }
-    
-    
+ */
     
 }
+
+// MARK: - Search Bar methods
+extension TodoListViewController : UISearchBarDelegate {
+    
+    // Query the database
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadDataItems(with: request)
+        
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        loadDataItems()
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadDataItems()
+            
+            //ask to the main thread to perform the going back to the original list
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
